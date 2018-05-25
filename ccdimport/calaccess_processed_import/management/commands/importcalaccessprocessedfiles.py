318682e9b1f549d75c6be5360a1d2a12132d5e50
andrewch8o <andc.gm@gmail.com>
2018-05-25 14:02:11 -0700

    Implemented "mapping" files in data directory to the django models

--- ccdimport/calaccess_processed_import/management/commands/importcalaccessprocessedfiles.py
+++ ccdimport/calaccess_processed_import/management/commands/importcalaccessprocessedfiles.py
@@ -4,6 +4,7 @@
 Import data from previously archived calaccess .csv files into a database after matching each file to the corresponding calaccess model by file name
 """
 import os
+from django.apps import apps
 from django.core.files import File
 from calaccess_raw import get_data_directory
 from calaccess_processed.management.commands import CalAccessCommand
@@ -21,7 +22,43 @@ class Command(CalAccessCommand):
         Get the model linked to this processed file record.
         """
         #TODO: implement model resolution
-        raise NotImplementedError
+        elections_model_dict = apps.get_app_config("calaccess_processed_elections").get_ocd_proxy_lookup()
+        #Filings lookup
+        model_list = apps.get_app_config("calaccess_processed_filings").get_filing_models()
+        filings_model_dict = dict((m.__name__, m) for m in model_list)
+
+        #Flatfiles lookup
+        flatfiles_model_dict = apps.get_app_config("calaccess_processed_flatfiles").get_flat_proxy_lookup()
+
+        resolved_model = None
+        if processed_file in elections_model_dict.keys():
+            resolved_model = elections_model_dict[processed_file]
+        elif processed_file in filings_model_dict.keys():
+            resolved_model = filings_model_dict[processed_file]
+        elif processed_file in flatfiles_model_dict.keys():
+            resolved_model = flatfiles_model_dict[processed_file]
+
+        return resolved_model
+
+    def list_models(self):
+        """
+        List the known models ( for debug purposes )
+        """
+        elections_model_dict = apps.get_app_config("calaccess_processed_elections").get_ocd_proxy_lookup()
+        self.log("ELECTIONS LOOKUP:")
+        for key in elections_model_dict.keys():
+            self.log("'{0}': '{1}'".format(key, elections_model_dict[key]))
+        #Filings lookup
+        model_list = apps.get_app_config("calaccess_processed_filings").get_filing_models()
+        filings_model_dict = dict((m.__name__, m) for m in model_list)
+        self.log("FILINGS LOOKUP:")
+        for key in filings_model_dict.keys():
+            self.log("'{0}': '{1}'".format(key, filings_model_dict[key]))
+
+        #Flatfiles lookup
+        flatfiles_model_dict = apps.get_app_config("calaccess_processed_flatfiles").get_flat_proxy_lookup()
+        for key in flatfiles_model_dict.keys():
+            self.log("'{0}': '{1}'".format(key, flatfiles_model_dict[key]))
 
     def handle(self, *args, **options):
         """
@@ -35,9 +72,14 @@ class Command(CalAccessCommand):
         )
         
         file_list = os.listdir(csv_dir)
+        self.get_model("")
         for fname in file_list:
             filename, file_extension = os.path.splitext(fname)
-            self.get_model(filename)
+            model = self.get_model(filename)
+            if model is None:
+                self.log("FAILED to resolve model for '{}'".format(filename))
+            else:
+                self.log("RESOLVED file: '{}'; model: '{}'".format(filename, model))
 
         #get files in the directory
         '''os.path.exists(csv_dir) or os.mkdir(csv_dir)
